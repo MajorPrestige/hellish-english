0614b5e184244a961d8c0ced16e3aa07ffdfaa53
Denys Dashkevych <denysdashkevych@gmail.com>
2023-03-07 16:31:35 +0200

    structured project

--- src/js/scroll-up.js
+++ src/js/scroll-up.js
@@ -1,12 +1,11 @@
 const scrollBtn = document.querySelector('.isShowBtn');
 window.onscroll = () => {
-    if(window.scrollY > 700){
-        scrollBtn.classList.remove('isShowBtn_hide');
-    }
-     else if(window.scrollY < 700){
-        scrollBtn.classList.add('isShowBtn_hide');
-    }
+  if (window.scrollY > 700) {
+    scrollBtn.classList.remove('isShowBtn_hide');
+  } else if (window.scrollY < 700) {
+    scrollBtn.classList.add('isShowBtn_hide');
+  }
 };
 scrollBtn.onclick = () => {
-    window.scrollTo(0, 0);
-}; 
+  window.scrollTo(0, 0);
+};
